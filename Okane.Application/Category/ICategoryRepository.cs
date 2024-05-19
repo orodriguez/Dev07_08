daa4b3d7c2b5c245ac003cb9e67724fceed00d5c
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-19 19:20:33 -0400

    _02_ICategoryRepository

--- Okane.Application/Category/ICategoryRepository.cs
+++ Okane.Application/Category/ICategoryRepository.cs
@@ -1,6 +1,8 @@
 ﻿namespace Okane.Application.Category;
-
-public interface ICategoryRepository
+// Nuestra interfaz para desplegar nuestros metodos Add All y ById
+public interface ICategoryRepository 
 {
-    
+    Domain.Category Add(Domain.Category category);
+    IEnumerable<Domain.Category> All();
+    Domain.Category? ById(int id);
 }
