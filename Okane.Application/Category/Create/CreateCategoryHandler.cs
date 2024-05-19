21098f68f26979a7db310460b55edb92ec89db49
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-19 19:29:59 -0400

    _08_CreateCategoryHandler

--- Okane.Application/Category/Create/CreateCategoryHandler.cs
+++ Okane.Application/Category/Create/CreateCategoryHandler.cs
@@ -1,6 +1,28 @@
-﻿namespace Okane.Application.Category.Create;
+﻿using FluentValidation;
+
+namespace Okane.Application.Category.Create;
 
 public class CreateCategoryHandler
 {
-    
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IValidator<CreateCategoryRequest> _validator;
+
+    public CreateCategoryHandler(IValidator<CreateCategoryRequest> validator, ICategoryRepository categoryRepository)
+    {
+        _validator = validator;
+        _categoryRepository = categoryRepository;
+    }
+
+    public ICategoryResponse Handle(CreateCategoryRequest createCategoryRequest)
+    {
+        var validation = _validator.Validate(createCategoryRequest);
+
+        if (!validation.IsValid)
+            return (ICategoryResponse)ValidationErrorsResponse.From(validation);
+
+        var category = createCategoryRequest.ToCategory();
+        var addedCategory = _categoryRepository.Add(category);
+
+        return CategorySuccessResponse.From(addedCategory);
+    }
 }
