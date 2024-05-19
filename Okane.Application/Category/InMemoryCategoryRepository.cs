edd731e71d3df6bb4efa249825726fab918bd34b
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-19 19:23:47 -0400

    _03_InMemoryCategoryRepository

--- Okane.Application/Category/InMemoryCategoryRepository.cs
+++ Okane.Application/Category/InMemoryCategoryRepository.cs
@@ -1,6 +1,22 @@
 ﻿namespace Okane.Application.Category;
 
-public class InMemoryCategoryRepository
+// Fake DB Para probar el codigo
+public class InMemoryCategoryRepository : ICategoryRepository
 {
-    
+    private readonly List<Domain.Category> _categories = new();
+    private int _nextId = 1;
+    public Domain.Category Add(Domain.Category category)
+    {
+        category.Id = _nextId++;
+        _categories.Add(category);
+        return category;
+    }
+    public IEnumerable<Domain.Category> All()
+    {
+        return _categories;
+    }
+    public Domain.Category? ById(int id)
+    {
+        return _categories.FirstOrDefault(category => category.Id == id);
+    }
 }
