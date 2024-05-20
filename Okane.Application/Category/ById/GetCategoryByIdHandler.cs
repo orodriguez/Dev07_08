8e098170e7b7a51f01c4f2f2ea1a676f481e11e2
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-19 23:43:56 -0400

    _16_GetCategoryByIdHandler.cs_GET '/categories/{id}'

---
+++ Okane.Application/Category/ById/GetCategoryByIdHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Okane.Application.Category.ById;
+
+public class GetCategoryByIdHandler
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public GetCategoryByIdHandler(ICategoryRepository categoryRepository) => 
+        _categoryRepository = categoryRepository;
+
+    public ICategoryResponse Handle(int id)
+    {
+        var expense = _categoryRepository
+            .ById(id);
+
+        if (expense == null)
+            return new NotFoundResponseCategory();
+        
+        return expense.ToCategoryResponse();
+    }
+}
