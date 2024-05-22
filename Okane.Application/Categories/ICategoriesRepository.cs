3754d9562c63c0375972c2a980aec6dd9d311356
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-21 23:44:06 -0400

    _02_ICategoriesRepository

--- Okane.Application/Categories/ICategoriesRepository.cs
+++ Okane.Application/Categories/ICategoriesRepository.cs
@@ -5,7 +5,14 @@ namespace Okane.Application.Categories;
 public interface ICategoriesRepository
 {
     Category? ByName(string categoryName);
+
     Category Add(Category category);
+
+    //
+    IList<Expense> GetExpensesByCategoryId(int categoryId);
+
+    //
+    void AddExpense(Expense expense);
     Category? ById(int id);
     bool NameExists(string name);
     bool Delete(Category category);
