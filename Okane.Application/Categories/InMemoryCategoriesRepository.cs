2ba539f320fe887e9a18f4d340d512c3331fa0f0
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-21 23:45:19 -0400

    _03_InMemoryCategoriesRepository

--- Okane.Application/Categories/InMemoryCategoriesRepository.cs
+++ Okane.Application/Categories/InMemoryCategoriesRepository.cs
@@ -5,12 +5,17 @@ namespace Okane.Application.Categories;
 public class InMemoryCategoriesRepository : ICategoriesRepository
 {
     private readonly IList<Category> _categories;
+    private readonly List<Expense> _expenses;
+
     private int _nextId = 1;
 
-    public InMemoryCategoriesRepository() => 
+    public InMemoryCategoriesRepository()
+    {
         _categories = new List<Category>();
+        _expenses = new List<Expense>();
+    }
 
-    public Category? ByName(string categoryName) => 
+    public Category? ByName(string categoryName) =>
         _categories.FirstOrDefault(c => c.Name == categoryName);
 
     public Category Add(Category category)
@@ -20,12 +25,21 @@ public class InMemoryCategoriesRepository : ICategoriesRepository
         return category;
     }
 
-    public Category? ById(int id) => 
+
+    public IList<Expense> GetExpensesByCategoryId(int categoryId) =>
+        _expenses.Where(e => e.CategoryId == categoryId).ToList();
+
+    public void AddExpense(Expense expense)
+    {
+        _expenses.Add(expense);
+    }
+
+    public Category? ById(int id) =>
         _categories.FirstOrDefault(c => c.Id == id);
 
-    public bool NameExists(string name) => 
+    public bool NameExists(string name) =>
         _categories.Any(c => c.Name == name);
 
-    public bool Delete(Category category) => 
+    public bool Delete(Category category) =>
         _categories.Remove(category);
 }
