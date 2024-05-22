4b8412ba0fef5ea8890c8a2405d38a4c9c29deed
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-21 23:52:08 -0400

    _08_GetCategoryByIdExpensesHandler

---
+++ Okane.Application/Categories/ById/GetCategoryByIdExpensesHandler.cs
@@ -0,0 +1,30 @@
+﻿using Okane.Application.Expenses;
+using Okane.Application.Responses;
+
+namespace Okane.Application.Categories.ById;
+
+public class GetCategoryByIdExpensesHandler
+{
+    private readonly ICategoriesRepository _categories;
+
+    public GetCategoryByIdExpensesHandler(ICategoriesRepository categories) =>
+        _categories = categories;
+
+    public IGetCategoryByIdExpensesResponse Handle(int categoryId)
+    {
+        var category = _categories.ById(categoryId);
+        if (category == null)
+            return new NotFoundResponse();
+
+        var expenses = _categories.GetExpensesByCategoryId(categoryId)
+            .Select(e => new ExpenseResponse(
+                e.Id,
+                e.Amount,
+                category.Name,
+                e.Description,
+                e.CreatedAt
+            )).ToList();
+
+        return new GetCategoryByIdExpensesResponse(category.Id, category.Name, expenses);
+    }
+}
