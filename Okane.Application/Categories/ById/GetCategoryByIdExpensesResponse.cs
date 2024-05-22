af18bb73a77202836c7aca109786e1245ec73c57
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-21 23:48:17 -0400

    _05_GetCategoryByIdExpensesResponse

---
+++ Okane.Application/Categories/ById/GetCategoryByIdExpensesResponse.cs
@@ -0,0 +1,6 @@
+﻿using Okane.Application.Expenses;
+
+namespace Okane.Application.Categories.ById;
+
+public record GetCategoryByIdExpensesResponse(int Id, string Name, List<ExpenseResponse> Expenses)
+    : IGetCategoryByIdExpensesResponse;
