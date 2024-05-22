3c1b88618fcfc6f283e33a1678ad27c257c6254f
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-21 23:59:56 -0400

    _12_CategoryResponse WithExpenses

--- Okane.Application/Categories/CategoryResponse.cs
+++ Okane.Application/Categories/CategoryResponse.cs
@@ -1,16 +1,21 @@
 using Okane.Application.Categories.ById;
 using Okane.Application.Categories.Create;
 using Okane.Application.Categories.Delete;
+using Okane.Application.Expenses;
 using Okane.Application.Responses;
 using Okane.Domain;
 
 namespace Okane.Application.Categories;
 
-public record CategoryResponse(int Id, string Name) : ISuccessResponse, 
-    ICreateCategoryResponse, 
+public record CategoryResponse(int Id, string Name) : ISuccessResponse,
+    ICreateCategoryResponse,
     IGetCategoryByIdResponse,
+    IGetCategoryByIdExpensesResponse,
     IDeleteCategoryResponse
 {
-    public static CategoryResponse From(Category category) => 
+    public static CategoryResponse From(Category category) =>
         new(category.Id, category.Name);
+
+    public static GetCategoryByIdExpensesResponse FromWithExpenses(Category category, List<ExpenseResponse> expenses) =>
+        new(category.Id, category.Name, expenses);
 }
