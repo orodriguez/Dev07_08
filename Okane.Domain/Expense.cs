226ffecfd2ff40c11c0bbd0c8827fa75e908c9da
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-23 17:46:57 -0400

    _01_Domain_Expenses_UserID

--- Okane.Domain/Expense.cs
+++ Okane.Domain/Expense.cs
@@ -4,6 +4,7 @@ public class Expense
 {
     public int Id { get; set; }
     public int Amount { get; set; }
+    public int UserId { get; set; }
     public required Category Category { get; set; }
     public string? Description { get; set; }
 
