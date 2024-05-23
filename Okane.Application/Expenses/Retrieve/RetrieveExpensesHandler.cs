16ae544083844bd556887b699d8f9ceeea7a2e4f
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-23 17:55:04 -0400

    _05_RetrieveExpensesHandler ByUserId

--- Okane.Application/Expenses/Retrieve/RetrieveExpensesHandler.cs
+++ Okane.Application/Expenses/Retrieve/RetrieveExpensesHandler.cs
@@ -1,14 +1,30 @@
+using Okane.Application.Auth;
+
 namespace Okane.Application.Expenses.Retrieve;
 
 public class RetrieveExpensesHandler
 {
     private readonly IExpensesRepository _expensesRepository;
+    private readonly IUserSession _userSession;
 
-    public RetrieveExpensesHandler(IExpensesRepository expensesRepository) => 
+    public RetrieveExpensesHandler(IExpensesRepository expensesRepository, IUserSession userSession)
+    {
         _expensesRepository = expensesRepository;
+        _userSession = userSession;
+    }
 
-    public IEnumerable<ExpenseResponse> Handle() =>
-        _expensesRepository
+
+    public IEnumerable<ExpenseResponse> Handle()
+    {
+        var userId = _userSession.GetCurrentUserId();
+        var expenses = _expensesRepository.ByUserId(userId);
+        return expenses.Select(expense => expense.ToExpenseResponse());
+
+        return null;
+        /*
+        return _expensesRepository
             .All()
             .Select(expense => expense.ToExpenseResponse());
+            */
+    }
 }
