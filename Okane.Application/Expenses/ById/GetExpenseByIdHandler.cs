522129b7aca97020d428d3123baa008bda7c9932
EnreiKazuri <vasquezmariano01@gmail.com>
2024-05-24 01:42:18 -0400

    Get Expense by Id is also restricted by userID

--- Okane.Application/Expenses/ById/GetExpenseByIdHandler.cs
+++ Okane.Application/Expenses/ById/GetExpenseByIdHandler.cs
@@ -1,3 +1,4 @@
+using Okane.Application.Auth;
 using Okane.Application.Responses;
 
 namespace Okane.Application.Expenses.ById;
@@ -5,14 +6,19 @@ namespace Okane.Application.Expenses.ById;
 public class GetExpenseByIdHandler
 {
     private readonly IExpensesRepository _expensesRepository;
+    private readonly IUserSession _userSession;
 
-    public GetExpenseByIdHandler(IExpensesRepository expensesRepository) => 
+    public GetExpenseByIdHandler(IExpensesRepository expensesRepository, IUserSession userSession)
+    {
         _expensesRepository = expensesRepository;
+        _userSession = userSession;
+    }
 
     public IResponse Handle(int id)
     {
         var expense = _expensesRepository
-            .ById(id);
+            .All()
+            .Where(e => e.Id == id && e.UserId == _userSession.GetCurrentUserId()).FirstOrDefault();
 
         if (expense == null)
             return new NotFoundResponse();
