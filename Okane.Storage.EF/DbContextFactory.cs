1238218284438e41d8ac555ac00c920570f23c5b
“orodriguez” <omarjavier15@gmail.com>
2024-05-24 16:01:48 -0400

    Read connection string from a config file in Storage.EF project

--- Okane.Storage.EF/DbContextFactory.cs
+++ Okane.Storage.EF/DbContextFactory.cs
@@ -1,15 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Okane.Storage.EF;
 
 public class DbContextFactory : IDesignTimeDbContextFactory<OkaneDbContext>
 {
+    private const string AppSettingsFileName = "appsettings.json";
+
     public OkaneDbContext CreateDbContext(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(AppSettingsFileName)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("Default");
+        
         var optionsBuilder = new DbContextOptionsBuilder<OkaneDbContext>();
-        optionsBuilder.UseNpgsql(
-            "Host=localhost;Port=5432;Database=OkaneDev;Username=postgres;Password=1234;");
+        optionsBuilder.UseNpgsql(connectionString);
 
         return new OkaneDbContext(optionsBuilder.Options);
     }
