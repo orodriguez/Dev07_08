fd433543c9cccf76305bf40505d9e2ebce19cb48
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:34:55 -0400

    _11_Budget -> BudgetValidator()

--- Okane.Application/Budget/Create/BudgetValidator.cs
+++ Okane.Application/Budget/Create/BudgetValidator.cs
@@ -4,5 +4,9 @@ namespace Okane.Application.Budget.Create;
 
 public class BudgetValidator : AbstractValidator<CreateBudgetRequest>
 {
-    
+    public BudgetValidator()
+    {
+        RuleFor(x => x.CategoryId).GreaterThan(0);
+        RuleFor(x => x.Maximum).GreaterThan(0);
+    }
 }
