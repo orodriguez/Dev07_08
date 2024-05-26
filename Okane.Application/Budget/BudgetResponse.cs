01a76d26a853c041b7053bc2419ecfbcceacd6c6
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:33:56 -0400

    _10_Budget ->  BudgetResponse From

--- Okane.Application/Budget/BudgetResponse.cs
+++ Okane.Application/Budget/BudgetResponse.cs
@@ -4,4 +4,12 @@ using Okane.Application.Responses;
 namespace Okane.Application.Budget;
 
 public record BudgetResponse(int CategoryId, int Maximum) : ISuccessResponse,
-    ICreateBudgetResponse;
+    ICreateBudgetResponse
+{
+    public static  BudgetResponse From(Domain.Budget budget) => 
+        new(budget.CategoryId ,budget.Maximum);
+}
+
+
+    
+    
