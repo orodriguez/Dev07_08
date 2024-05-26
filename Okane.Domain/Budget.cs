fd3ac44a65fbfbe152b410825145913a7bc7972d
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 11:42:16 -0400

    _20_ Domain -> Budget ->   public Category Category { get; set; } = null!;

--- Okane.Domain/Budget.cs
+++ Okane.Domain/Budget.cs
@@ -5,4 +5,5 @@ public class Budget
     public int Id { get; set; }
     public int CategoryId { get; set; }
     public int Maximum { get; set; }
+    public Category Category { get; set; } = null!;
 }
