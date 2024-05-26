0f153960c0f7a8eec69de82785c001bf8c906a69
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:41:30 -0400

    _13_Okane.Storage.EF -> BudgetsRepository

---
+++ Okane.Storage.EF/BudgetsRepository.cs
@@ -0,0 +1,21 @@
+﻿using Okane.Application.Budget;
+using Okane.Domain;
+
+namespace Okane.Storage.EF;
+
+public class BudgetsRepository : IBudgetRepository
+{
+    private readonly OkaneDbContext _db;
+    public BudgetsRepository(OkaneDbContext db) => _db = db;
+
+    public void Add(Budget budget)
+    {
+        _db.Budgets.Add(budget);
+        _db.SaveChanges();
+        //  throw new NotImplementedException();
+    }
+
+    public Budget? GetByCategoryId(int categoryId) =>
+        _db.Budgets.FirstOrDefault(b => b.CategoryId == categoryId);
+    //  throw new NotImplementedException();
+}
