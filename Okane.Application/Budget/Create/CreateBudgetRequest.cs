e01dd807c41860f175d725184da3e2c1388f7d61
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:22:47 -0400

    _05_Budget -> CreateBudgetRequest

--- Okane.Application/Budget/Create/CreateBudgetRequest.cs
+++ Okane.Application/Budget/Create/CreateBudgetRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace Okane.Application.Budget.Create;
 
-public class CreateBudgetRequest
+public class CreateBudgetRequest(int categoryId, int Maximum)
 {
-    
+    public Domain.Budget ToBudget() =>
+        new() { CategoryId = categoryId, Maximum = Maximum };
 }
