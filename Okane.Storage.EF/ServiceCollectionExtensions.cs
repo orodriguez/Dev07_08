ecbcca94bbbd2b933711767434f9b68a19dbb125
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:47:40 -0400

    _16_ Okane.Storage.EF -> ServiceCollectionExtensions

--- Okane.Storage.EF/ServiceCollectionExtensions.cs
+++ Okane.Storage.EF/ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Okane.Application.Auth;
+using Okane.Application.Budget;
+using Okane.Application.Budget.Create;
 using Okane.Application.Categories;
 using Okane.Application.Expenses;
 
@@ -9,7 +12,8 @@ namespace Okane.Storage.EF;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddOkaneEFStorage(this IServiceCollection services, ConfigurationManager configuration)
+    public static IServiceCollection AddOkaneEFStorage(this IServiceCollection services,
+        ConfigurationManager configuration)
     {
         var connectionString = configuration.GetConnectionString("Default");
         services.AddDbContext<OkaneDbContext>(options => options.UseNpgsql(connectionString));
@@ -17,6 +21,8 @@ public static class ServiceCollectionExtensions
         services.AddTransient<IReadOnlyExpensesRepository, ExpensesRepository>();
         services.AddTransient<ICategoriesRepository, CategoriesRepository>();
         services.AddTransient<IUsersRepository, UsersRepository>();
+        services.AddTransient<IBudgetRepository, BudgetsRepository>();
+        services.AddTransient<IValidator<CreateBudgetRequest>, BudgetValidator>();
         return services;
     }
 }
