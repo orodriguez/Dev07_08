b6479df54d65f8a09abef3ac1533e46c69c38720
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:25:39 -0400

    _06_Budget -> CreateBudgetHandler

--- Okane.Application/Budget/Create/CreateBudgetHandler.cs
+++ Okane.Application/Budget/Create/CreateBudgetHandler.cs
@@ -1,6 +1,34 @@
-﻿namespace Okane.Application.Budget.Create;
+﻿using FluentValidation;
+using Okane.Application.Responses;
+
+
+namespace Okane.Application.Budget.Create;
 
 public class CreateBudgetHandler
 {
-    
+    private readonly IBudgetRepository _budget;
+    private readonly IValidator<CreateBudgetRequest> _validator;
+
+    public CreateBudgetHandler(IBudgetRepository budget, IValidator<CreateBudgetRequest> validator) =>
+        (_budget, _validator) = (budget, validator);
+
+    public ICreateBudgetResponse Handle(CreateBudgetRequest request)
+    {
+        var result = _validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            return ValidationErrorsResponse.From(result);
+        }
+
+        var existingBudget = _budget.GetByCategoryId(request.CategoryId);
+        if (existingBudget != null)
+        {
+            return new ConflictResponse($"Budget for category ID '{request.CategoryId}' already exists.");
+        }
+
+        var budget = request.ToBudget();
+        _budget.Add(budget);
+        return BudgetResponse.From(budget);
+    }
 }
