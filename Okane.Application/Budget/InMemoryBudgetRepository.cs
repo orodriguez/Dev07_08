0bfff66c26ff4141bc25e11ac2068860629a36d8
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-26 08:20:10 -0400

    _03_Budget -> InMemoryBudgetRepository

--- Okane.Application/Budget/InMemoryBudgetRepository.cs
+++ Okane.Application/Budget/InMemoryBudgetRepository.cs
@@ -2,4 +2,16 @@
 
 public class InMemoryBudgetRepository : IBudgetRepository
 {
+    private readonly IList<Domain.Budget> _budget;
+
+    public InMemoryBudgetRepository() =>
+        _budget = new List<Domain.Budget>();
+
+    public void Add(Domain.Budget budget)
+    {
+        _budget.Add(budget);
+    }
+
+    public Domain.Budget? GetByCategoryId(int categoryId) =>
+        _budget.FirstOrDefault(b => b.CategoryId == categoryId);
 }
